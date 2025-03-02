1e27404a7d14925d1f5583a7aa334821c91cd844
reshma-devraj <reshmadevraj2230@gmail.com>
2025-03-02 20:44:54 +0530

    done

---
+++ project/sricpt.js
@@ -0,0 +1,7 @@
+function sayHello() {
+    document.getElementById("message").textContent = "GOOD MORNING! Hope you have a great day! 😊";
+}
+
+function hideNo() {
+    document.getElementById("no-btn").style.display = "none";
+}
